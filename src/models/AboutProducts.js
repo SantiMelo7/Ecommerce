f58c68da6954a81c42454c761a3b1af2121c3486
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-04 19:37:02 -0600

    Changes

--- src/models/AboutProducts.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model, models } from "mongoose"
-
-const AboutsProductsSchema = new Schema({
-    images: { type: String, required: true },
-}, { timestamps: true })
-
-export const AboutProducts = models?.AboutProducts || model("AboutProducts", AboutsProductsSchema)
