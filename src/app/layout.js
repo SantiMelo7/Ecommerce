33ab932524da84499a876004307152d99c607e4b
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-05 15:56:52 -0600

    Añadiendo el favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,30 @@
 import { Oswald } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
+import AppProvider from '@/components/AppProvider'
 
 const oswald = Oswald({ subsets: ['cyrillic'], weight: ["400"] })
 
 export const metadata = {
   title: 'Ecommerce Technology',
   description: '',
+  icons: {
+    icon: ["favicon.ico?v=4"],
+    apple: ["apple-touch-icon.png?v=4"],
+    shortcut: ["apple-touch-icon.png"]
+  }
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ session, children }) {
   return (
     <html lang="en">
       <body className={oswald.className}>
-        <div className="bg-gray-800 -z-10 pb-5 w-screen">
-          <Header />
-        </div>
-        {children}
+        <AppProvider session={session}>
+          <div className="bg-gray-800 -z-10 pb-5 w-screen">
+            <Header />
+          </div>
+          {children}
+        </AppProvider>
       </body>
     </html>
   )
