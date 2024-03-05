4f5f4ac8df54679bde46cf899efc45cc633c4f9b
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-04 19:37:19 -0600

    Model de las imagenes

--- src/app/api/aboutProducts/route.js
+++ src/app/api/aboutProducts/route.js
@@ -1,16 +1,16 @@
-import { AboutProducts } from "@/models/AboutProducts"
+import { ImagesSchema } from "@/models/ImagesAbout"
 import { connectMongoDB } from "@/util/connectMongoDB"
 
 export async function POST(req) {
     await connectMongoDB()
     const data = await req.json()
-    const doc = await AboutProducts.create(data)
+    const doc = await ImagesSchema.create(data)
     return Response.json(doc)
 }
 
 export async function GET() {
     await connectMongoDB()
-    const doc = await AboutProducts.find()
+    const doc = await ImagesSchema.find()
     return Response.json(doc)
 }
 
@@ -18,6 +18,6 @@ export async function DELETE(req) {
     await connectMongoDB()
     const url = new URL(req.url)
     const _id = url.searchParams.get("_id")
-    await AboutProducts.deleteOne({ _id })
+    await ImagesSchema.deleteOne({ _id })
     return Response.json(true)
 }
