ab0d6ce483fc7f975021c0940d6031e3187501a4
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-06 21:57:35 -0600

    Menú

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,18 @@
 import ListMenu from "./ListMenu";
+import Session from "./Sesion";
 
 export default function Header() {
   return (
-    <header className="flex w-screen h-full top-2 relative md:justify-between text-white text-lg overflow-auto">
+    <header className="flex w-screen h-full top-2 relative md:justify-between text-white text-lg overflow-hidden">
       <div className="pl-5">
         <h1 className="text-2xl">
           <a href="/">Ecommerce</a>
         </h1>
       </div>
-      <nav className="flex flex-grow md:justify-end md:items-end sm:flex-col relative ">
+      <nav className="flex justify-center items-center flex-grow basis-0">
         <ListMenu />
       </nav>
+      <Session />
     </header>
   );
 }
--- src/components/layout/ListMenu.jsx
+++ src/components/layout/ListMenu.jsx
@@ -1,9 +1,13 @@
-import Session from "./Sesion";
+"use client";
+
+import { useCart } from "@/hooks-reducer/cart";
+import { CartIcon } from "../reutilizable/Icons";
 
 export default function ListMenu() {
+  const { cart } = useCart();
   return (
     <>
-      <ul className="flex gap-x-4 cursor-pointer relative right-8">
+      <ul className="flex gap-x-4 text-xl">
         <li>
           <a href="/">Home</a>
         </li>
@@ -14,10 +18,14 @@ export default function ListMenu() {
           <a href="/products">All Products</a>
         </li>
         <li>
-          <a href="/cart">Cart</a>
-        </li>
-        <li>
-          <Session />
+          <a href="/cart" className="relative left-20">
+            <CartIcon />
+          </a>
+          <div className="bg-blue-200 h-[2vh] w-4 rounded-[20px] relative left-6 -top-7">
+            <div className="relative text-black flex justify-center items-center text-sm">
+              {cart}
+            </div>
+          </div>
         </li>
       </ul>
     </>
