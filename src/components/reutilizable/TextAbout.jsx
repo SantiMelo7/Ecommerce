f74420e27b4f41bf787a06074f429a7ef23a30d2
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-07 19:51:32 -0600

    Texto actualizado del about

--- src/components/reutilizable/TextAbout.jsx
+++ src/components/reutilizable/TextAbout.jsx
@@ -1,19 +1,33 @@
 export default function TextAbout() {
   return (
-    <>
-      <span className="span-about">
-        In the heart of the bustling city center lies a gastronomic oasis known
-        as Lorem Ipsum. This charming restaurant, with its exposed brick walls
-        and soft lighting, invites you to immerse yourself in an unforgettable
-        culinary experience from the moment you cross its threshold.
+    <div className="flex flex-col gap-y-2">
+      <span className="span-about md:text-gray-700 sm:text-gray-200 text-lg">
+        Sure, here&apos;s a brief description of a technology store in English:
+        Welcome to Lorem your one-stop destination for all things tech! Nestled
+        in the heart of downtown, Lorem boasts a vast array of cutting-edge
+        gadgets and gizmos to satisfy even the most discerning tech enthusiasts.
+        From the latest smartphones and sleek laptops to state-of-the-art smart
+        home devices and immersive VR gear, we&apos;ve got you covered.
       </span>
-      <span className="span-about">
-        With décor that evokes the warmth and familiarity of home, Lorem Ipsum
-        offers a welcoming atmosphere that is perfect for any occcasion, whether
-        it is an intimate dinner for two or a group celebration. The tables are
-        adorned with crisp white tablecloths, and flickering candles add a touch
-        of romance to the atmosphere.
+      <span className="span-about text-lg">
+        Step into our sleek, modern showroom where knowledgeable staff members
+        are on hand to guide you through the latest advancements in technology.
+        Whether you&apos;re a seasoned tech guru or just dipping your toes into
+        the digital world, our friendly experts are here to help you find the
+        perfect device to fit your needs.
       </span>
-    </>
+      <span className="span-about md:flex sm:hidden text-gray-700  text-lg">
+        But we&apos;re not just about selling gadgets at Lorem, we&apos;re
+        passionate about fostering a tech-savvy community. That&apos;s why we
+        offer regular workshops and events where customers can learn new skills,
+        discover emerging trends, and connect with like-minded tech enthusiasts.
+      </span>
+      <span className="span-about sm:hidden">
+        So whether you&apos;re in the market for a new smartphone, craving the
+        latest gaming console, or simply want to geek out over the newest tech
+        innovations, come experience the excitement at Lorem where technology
+        meets community.
+      </span>
+    </div>
   );
 }
