c17c9f1b8eee356efef8f497fb62187660507f58
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-07 19:52:38 -0600

    Añadiendo los colores que por ahora seran, (no es la idea final)

--- src/components/reutilizable/MainComponent.jsx
+++ src/components/reutilizable/MainComponent.jsx
@@ -1,22 +1,26 @@
 import HeroMenu from "./HeroMenu";
 import MainTitle from "./MainTitle";
 import MenuPost from "../products/MenuPost";
-import AboutRestaurant from "../about/AboutRestaurant";
 import Contact from "../contact/Contact";
 import Footer from "../footer/Footer";
+import AboutTec from "../about/AboutTec";
 
 export default function MainComponent() {
   return (
     <>
-      <div className=" bg-gray-800 relative -top-1">
+      <div className=" bg-gray-800 relative -top-1 mb-3">
         <HeroMenu />
       </div>
-      <div className="md:mt-10 sm:mt-24">
+      <div className="relative -top-4 bg-[#A7E4F1] pb-8 ">
         <MainTitle title="Products" subtTitle="Some of the products we have" />
-        <MenuPost className="shadow-blue-100 bg-[#A5C2E2] hover:bg-[#57BBE9]" />
+        <MenuPost className="shadow-gray-800 bg-[#A5C2E2] hover:bg-[#57BBE9] animate-flip-x" />
       </div>
-      <AboutRestaurant />
-      <Contact />
+      <div className="relative -top-4 bg-gradient-to-r from-[#f56565] to-[#9b2c2c]">
+        <AboutTec />
+      </div>
+      <div className="relative -top-4">
+        <Contact />
+      </div>
       <Footer />
     </>
   );
