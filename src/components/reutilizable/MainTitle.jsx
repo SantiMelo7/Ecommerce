a1867fe0d1ff7d84dfbfa569c80f2485fe8fb860
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-07 19:50:27 -0600

    Título

--- src/components/reutilizable/MainTitle.jsx
+++ src/components/reutilizable/MainTitle.jsx
@@ -1,7 +1,7 @@
 function MainTitle({ title, subtTitle }) {
   return (
-    <div className="flex flex-col justify-center items-center md:max-w-3xl sm:max-w-sm sm:pl-3 sm:pr-4 mx-auto mt-8">
-      <h1 className="text-5xl text-red-500 font-extrabold text-center">
+    <div className="flex flex-col justify-center items-center md:max-w-3xl sm:max-w-sm sm:pl-3 sm:pr-4 mx-auto ">
+      <h1 className="text-5xl text-green-500 font-extrabold text-center mt-8">
         {title}
       </h1>
       <h3 className="mt-3 text-2xl uppercase text-center md:mb-2 sm:mb-5">
