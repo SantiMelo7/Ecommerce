b55c8ef21dd123c1032f32fbd57e35781cbc8ae3
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 18:59:17 -0600

    Añadiendo la petición de el pedido

--- src/components/cart/FormCart.jsx
+++ src/components/cart/FormCart.jsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AddressForm from "../form/AddressForm";
+import { CartContext } from "@/context/AppProvider";
+import ButtonPay from "./ButtonPay";
 
 export default function FormCart() {
   const [address, setAddress] = useState({});
+
+  const { cartProducts } = useContext(CartContext);
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        address,
+        cartProducts,
+      }),
+    });
+    window.location = await response.json();
+  }
 
   function handleChangeAddress(propName, value) {
     setAddress((prevAddress) => ({ ...prevAddress, [propName]: value }));
   }
   return (
-    <form className="flex flex-col md:justify-end md:items-end mt-10">
+    <form
+      className="flex flex-col md:justify-end md:items-end md:right-10 md:relative sm:max-w-screen-md"
+      onSubmit={handleSubmit}
+    >
       <h1 className="text-3xl text-center">Checkout</h1>
-      <AddressForm
-        setAddressChange={address}
-        itemAddress={handleChangeAddress}
-      />
-      <div className="flex justify-center items-center mx-auto mb-3">
-        <button className="bg-red-500 text-white py-3 px-3 w-52 rounded-md text-xl">
-          Pay $
-        </button>
-      </div>
+      <AddressForm itemAddress={handleChangeAddress} setAddressProp={address} />
+      <ButtonPay />
     </form>
   );
 }
