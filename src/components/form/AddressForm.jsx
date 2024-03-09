5fd5b32b236f43c2a08dfef4899e4f8a03d28285
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 18:59:46 -0600

    Estilos responsive form

--- src/components/form/AddressForm.jsx
+++ src/components/form/AddressForm.jsx
@@ -1,41 +1,41 @@
 import InputCreate from "./InputCreate";
 
-export default function AddressForm({ itemAddress, setAddressChange }) {
+export default function AddressForm({ itemAddress, setAddressProp }) {
   const { phone, streetAddress, postalCode, city, country } = itemAddress;
   return (
-    <>
+    <div className="flex flex-col justify-center items-center">
       <InputCreate
         label="Phone Number"
         value={phone}
-        className="w-[70vh]"
-        onChange={(ev) => setAddressChange("phone", ev.target.value)}
+        className="md:w-[70vh] sm:w-[40vh]"
+        onChange={(ev) => setAddressProp("phone", ev.target.value)}
       />
       <InputCreate
         label="Street Address"
         value={streetAddress}
-        className="w-[70vh]"
-        onChange={(ev) => setAddressChange("streetAddress", ev.target.value)}
+        className="md:w-[70vh] sm:w-[40vh]"
+        onChange={(ev) => setAddressProp("streetAddress", ev.target.value)}
       />
-      <div className="grid grid-cols-2 mx-auto relative left-20 gap-x-3">
+      <div className="md:grid md:grid-cols-2 mx-auto relative left-2 gap-x-3">
         <InputCreate
           label="Postal Code"
-          className="w-[35vh]"
+          className="md:w-[35vh] sm:w-[40vh]"
           value={postalCode}
-          onChange={(ev) => setAddressChange("postalCode", ev.target.value)}
+          onChange={(ev) => setAddressProp("postalCode", ev.target.value)}
         />
         <InputCreate
           label="City"
-          className="w-[35vh]"
+          className="md:w-[35vh] sm:w-[40vh]"
           value={city}
-          onChange={(ev) => setAddressChange("city", ev.target.value)}
+          onChange={(ev) => setAddressProp("city", ev.target.value)}
         />
       </div>
       <InputCreate
         label="Country"
         value={country}
-        className="w-[70vh]"
-        onChange={(ev) => setAddressChange("country", ev.target.value)}
+        className="md:w-[70vh] sm:w-[40vh]"
+        onChange={(ev) => setAddressProp("country", ev.target.value)}
       />
-    </>
+    </div>
   );
 }
