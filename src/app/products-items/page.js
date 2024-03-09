cd23f09f7cb9c79230f16609ab8747c595e011ee
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 20:32:37 -0600

    Eliminado el componente del about

--- src/app/products-items/page.js
+++ src/app/products-items/page.js
@@ -1,6 +1,5 @@
 "use client"
 
-import AboutPost from "@/components/about/AboutPost"
 import LinkProducts from "@/components/products/LinksProducts"
 import MenuPost from "@/components/products/MenuPost"
 import NavTabs from "@/components/reutilizable/NavTabs"
@@ -11,8 +10,6 @@ export default function ProductsPage() {
             <NavTabs admin={true} />
             <LinkProducts href={"/products-items/new"} text="Crear Un Nuevo Producto" />
             <MenuPost className="shadow-green-400 bg-[#ADE1B3] hover:bg-[#2FC13E]" />
-            <LinkProducts href={"/about/new"} text="Añade imagenes del restaurante" />
-            <AboutPost />
         </section>
     )
 }
