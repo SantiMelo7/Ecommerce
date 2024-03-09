b30e75f30fc1a1d136cb6588d1a9e9e1d43c3a2d
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 19:00:05 -0600

    Styles

--- src/components/form/GenericInput.jsx
+++ src/components/form/GenericInput.jsx
@@ -1,5 +1,4 @@
-export const cssLabelInput =
-  "text-2xl text-gray-600 mb-2 font-bold uppercase mt-3";
+export const cssLabelInput = "text-2xl text-gray-600  font-bold uppercase mt-3";
 
 export default function GenericInput({
   label,
