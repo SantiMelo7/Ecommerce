64cb923d0d14cc0fa2a9892817d9de89f4b64d09
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 18:59:58 -0600

    Quitando el slice

--- src/components/products/MenuPost.jsx
+++ src/components/products/MenuPost.jsx
@@ -5,12 +5,11 @@ import MenuItemProps from "./MenuItemProps";
 
 export default function MenuPost({ className }) {
   const { products } = usePostProducts();
-  const productSlice = products.slice(0, 8);
 
   return (
     <section className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 md:max-w-screen-xl md:mt-10 sm:max-w-screen-sm sm:mx-auto">
-      {productSlice.length > 0 &&
-        productSlice.map((text) => (
+      {products.length > 0 &&
+        products.map((text) => (
           <div key={text.id}>
             <MenuItemProps className={className} {...text} />
           </div>
