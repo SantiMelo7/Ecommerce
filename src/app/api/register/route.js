9963e2b62603bc3c96f598144461d63f2609848e
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 19:00:18 -0600

    Actualizando el nombre del model

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,10 +1,10 @@
 import { connectMongoDB } from "@/util/connectMongoDB"
-import { UserItem } from "../../../models/UserItem"
+import { User } from "../../../models/User"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
     const { name, email } = await req.json()
     await connectMongoDB()
-    await UserItem.create({ name, email })
+    await User.create({ name, email })
     return NextResponse.json({ message: "Felicidades, has iniciado sesion" }, { status: 201 })
 }
