21d2e84c33d4fbb642718570f1b225ead317840b
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 20:43:21 -0600

    Componente de sponsor fake

---
+++ src/components/footer/SponsorsFake.jsx
@@ -0,0 +1,7 @@
+export default function SponsorsFake({ img }) {
+  return (
+    <div className="flex border-[2px] border-gray-700 px-4 md:w-44 sm:w-32 bg-gray-300 justify-center items-center">
+      {img}
+    </div>
+  );
+}
