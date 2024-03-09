5206e025e83b339dd19e13b5e17ecf943acd75af
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 19:00:25 -0600

    Eliminado

--- src/models/UserItem.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model, models } from "mongoose"
-
-const UserItemsSchema = new Schema({
-    email: { type: String, required: true },
-    name: { type: String, required: true }
-}, { timestamps: true })
-
-export const UserItem = models?.UserItem || model("users-page", UserItemsSchema)
