d2f451b798568d47160324f80d2f9847453a10a6
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-08 20:31:51 -0600

    Eliminando el about anterior

--- src/models/ImagesAbout.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model, models } from "mongoose"
-
-export const ImagesSchema = new Schema({
-
-    images: { type: String, required: true },
-
-}, { timestamps: true })
-
-export const Images = models?.Images || model("images-about", ImagesSchema)
