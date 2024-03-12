5cae29ef64d1ee383fcf5703e4ec1d336ec3be65
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-12 14:46:55 -0600

    Añadiendo el slice

--- src/components/products/MenuPost.jsx
+++ src/components/products/MenuPost.jsx
@@ -6,14 +6,16 @@ import MenuItemProps from "./MenuItemProps";
 export default function MenuPost({ className }) {
   const { products } = usePostProducts();
 
+  const productsSlice = products.slice(0, 8);
+
   return (
-    <section className="grid xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-2 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 md:max-w-screen-2xl md:mt-10 sm:max-w-screen-sm sm:mx-auto">
-      {products.length > 0 &&
-        products.map((text) => (
-          <div key={text.id}>
+    <>
+      <section className="grid xl:grid-cols-4 md:grid-cols-2 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 md:max-w-screen-2xl md:mt-10 sm:max-w-screen-sm sm:mx-auto">
+        {productsSlice.length > 0 &&
+          productsSlice.map((text) => (
             <MenuItemProps className={className} {...text} />
-          </div>
-        ))}
-    </section>
+          ))}
+      </section>
+    </>
   );
 }
