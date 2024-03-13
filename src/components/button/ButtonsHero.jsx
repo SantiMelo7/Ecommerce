48243d006a5c293948cb1ced8dae048deb5a9e91
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-12 22:31:06 -0600

    Boton de hero

---
+++ src/components/button/ButtonsHero.jsx
@@ -0,0 +1,9 @@
+export default function ButtonHero() {
+  return (
+    <div className="text-white text-lg mt-4 mb-10 flex justify-center gap-x-10">
+      <button className="border-[1px] border-white px-6 hover:transition hover:scale-105">
+        Read More
+      </button>
+    </div>
+  );
+}
