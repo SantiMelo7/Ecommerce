e2a5b652976f63e1f853ccf624bd045ba549ff21
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-12 22:31:46 -0600

    Dando el componente del boton, eliminar y editar

--- src/components/form/form-categories/CategoriesPost.jsx
+++ src/components/form/form-categories/CategoriesPost.jsx
@@ -1,42 +1,30 @@
 "use client";
 
-import { DeleteIcon, EditIcon } from "../../layout/Icons";
 import useCategoriesRequest from "@/hooks/useCategoriesRequest";
+import ButtonsCategories from "../../button/ButtonsCategories";
 
 export default function CategoriesPost() {
-  const { categories, setCategoriesName, setEdited, handleDelete } =
-    useCategoriesRequest();
+  const { categories } = useCategoriesRequest();
 
   return (
     <section className="mt-7 grid grid-cols-3 gap-x-5 hover:transition">
       {categories?.length > 0 &&
         categories.map((text) => (
-          <>
-            <div
-              key={text._id}
-              className="flex flex-col items-center mb-4 bg-orange-300  hover:scale-105 rounded-md"
-            >
-              <div className="grow rounded-md">
-                <h2 className="text-gray-800 text-xl font-bold mb-3 pl-3 pt-3">
-                  {text.name}
-                </h2>
-              </div>
-              <div className="flex gap-8 justify-center items-center">
-                <button
-                  className="w-8 h-11"
-                  onClick={() => {
-                    setEdited(text), setCategoriesName(text.name);
-                  }}
-                >
-                  {" "}
-                  <EditIcon />
-                </button>
-                <button className="pr-4" onClick={() => handleDelete(text._id)}>
-                  <DeleteIcon />
-                </button>
-              </div>
+          <div
+            key={text._id}
+            className="flex flex-col items-center mb-4 bg-orange-300  hover:scale-105 rounded-md"
+          >
+            <div className="grow rounded-md">
+              <h2 className="text-gray-800 text-xl font-bold mb-3 pl-3 pt-3">
+                {text.name}
+              </h2>
             </div>
-          </>
+            <ButtonsCategories
+              textName={text}
+              name={text.name}
+              deleteName={text._id}
+            />
+          </div>
         ))}
     </section>
   );
