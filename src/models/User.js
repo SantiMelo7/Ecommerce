ed868738cfe1252e89ca6a5aa26d813cd4a21ba6
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-12 22:32:03 -0600

    Agregando mas valores al User

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
 
 const UserSchema = mongoose.Schema({
+    name: { type: String, required: true },
     email: { type: String, required: true },
-    name: { type: String, required: true }
+    phone: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+    streetAddress: { type: String, required: true },
+    city: { type: String, required: true },
+    admin: { type: Boolean, required: true }
 }, { timestamps: true })
 
 export const UserItem = mongoose.models?.UserItemGoogle || mongoose.model("UserItemGoogle", UserSchema)
