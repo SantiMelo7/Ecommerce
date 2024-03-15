8bfd548ea71c520f77bdf5d0a963471ecf06551b
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-15 14:45:10 -0600

    Delete en el api de users

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -6,3 +6,11 @@ export async function GET() {
     const users = await UserItem.find()
     return Response.json(users)
 }
+
+export async function DELETE(req) {
+    await connectMongoDB()
+    const url = new URL(req.url)
+    const _id = url.searchParams.get("_id")
+    await ProductsItem.deleteOne({ _id })
+    return Response.json(true)
+}
