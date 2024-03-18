fec65368c9070b2c53e8fc9a5bfac842921bbb30
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 16:22:53 -0600

    Mas atributos a la orden

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,11 +1,14 @@
 import { Schema, model, models } from "mongoose"
 
 const OrderSchema = new Schema({
+    userEmail: { type: String, required: true },
     phone: { type: String, required: true },
     postalCode: { type: String, required: true },
     country: { type: String, required: true },
     streetAddress: { type: String, required: true },
     city: { type: String, required: true },
+    cartProducts: { type: Boolean, required: true },
+    paid: { type: Boolean, required: true }
 }, { timestamps: true })
 
 export const Order = models?.Order || model("order-items", OrderSchema)
