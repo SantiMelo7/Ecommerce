8a9bb0510af65b548f9e24d0dd85a883466a295f
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 16:34:58 -0600

    Quitando state

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useState } from "react"
-import { redirect, useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 
 export function usePostProducts() {
     const [products, setProducts] = useState([])
@@ -14,11 +14,13 @@ export function usePostProducts() {
     return { products }
 }
 
-export function ProductRequest() {
+export function useProductRequest() {
 
     const { id } = useParams()
     const [product, setProduct] = useState(null)
-    const [redirectSubmit, setRedirectSubmit] = useState(false)
+    const [error, setError] = useState(null)
+    const router = useRouter()
+    const routerProduct = router.push("/products-items")
 
     useEffect(() => {
         fetch("/api/products").then(response => {
@@ -29,48 +31,43 @@ export function ProductRequest() {
         })
     }, [])
 
+    async function handleFormSubmit(ev, data) {
+        ev.preventDefault()
+        try {
+            const response = await fetch("/api/products", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+            setError(false)
+            routerProduct()
+        } catch (error) {
+            setError(error)
+            router.push("/products-items/new")
+        }
+    }
+
     async function handleSubmitEdit(ev, data) {
         ev.preventDefault()
-        setRedirectSubmit(false)
         data = { ...data, _id: id }
-        const response = await fetch("/api/products", {
-            method: "PUT", headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
-        })
-        setRedirectSubmit(true)
+        try {
+            const response = await fetch("/api/products", {
+                method: "PUT", headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+            setError(false)
+            routerProduct()
+        } catch (error) {
+            setError(error)
+        }
     }
 
     async function handleDelete() {
         const response = await fetch("/api/products?_id=" + id, {
             method: "DELETE"
         })
-        setRedirectSubmit(true)
-    }
-    if (redirectSubmit) {
-        return redirect("/products-items")
-
+        routerProduct()
     }
 
-    return { product, handleSubmitEdit, handleDelete }
-
-}
-
-export function NewProduct() {
-
-    const [redirectOnSubmit, setRedirectOnSubmit] = useState(false)
-
-    async function handleFormSubmit(ev, data) {
-        ev.preventDefault()
-        const response = await fetch("/api/products", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-        setRedirectOnSubmit(true)
-    }
-
-    if (redirectOnSubmit) {
-        return redirect("/products-items")
-    }
-    return { handleFormSubmit }
+    return { product, handleSubmitEdit, handleDelete, handleFormSubmit, error }
 }
