79aae27049e2f4bf215e5da933aa12ca6d0ca976
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 15:56:02 -0600

    Damos que si el config tiene el slice, mostrara los productos que se pongan al dar esa propiedad

--- src/components/products/MenuPost.jsx
+++ src/components/products/MenuPost.jsx
@@ -3,18 +3,20 @@
 import { usePostProducts } from "@/hooks/useProducts";
 import MenuItemProps from "./MenuItemProps";
 
-export default function MenuPost({ className }) {
+export default function MenuPost({ className, config }) {
   const { products } = usePostProducts();
-
-  const productsSlice = products.slice(0, 8);
+  let productsToDisplay = products;
+  if (config.slice) {
+    productsToDisplay = products.slice(0, config.slice);
+  }
 
   return (
     <>
       <section className="grid xl:grid-cols-4 md:grid-cols-2 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 md:max-w-screen-2xl md:mt-10 sm:max-w-screen-sm sm:mx-auto">
-        {productsSlice.length > 0 &&
-          productsSlice.map((text) => (
+        {productsToDisplay.length > 0 &&
+          productsToDisplay.map((text) => (
             <div key={text._id}>
-              <MenuItemProps className={className} {...text} />
+              <MenuItemProps className={className} config={config} {...text} />
             </div>
           ))}
       </section>
