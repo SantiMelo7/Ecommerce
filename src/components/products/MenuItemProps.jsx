b0cf0a62c3d10c6734bb787139fa6db7c16c70e6
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 15:55:22 -0600

    Añadiendo como prop el config y dando la condicional de que si es true muestre los valores de los products

--- src/components/products/MenuItemProps.jsx
+++ src/components/products/MenuItemProps.jsx
@@ -3,24 +3,37 @@ import CartButton from "../button/CartButton";
 import { useContext } from "react";
 import { CartContext } from "@/context/AppProvider";
 
-function MenuItemProps({ className, ...menuItem }) {
+function MenuItemProps({ className, config, ...menuItem }) {
   const { images, name, price, description, category } = menuItem;
   const { addToCart } = useContext(CartContext);
 
   return (
     <div className={`flex flex-col shadow-lg ${className}`}>
-      <Image
-        width={192}
-        height={144}
-        src={images}
-        alt="Images"
-        className="rounded-lg mx-auto mt-3"
-      />
-      <h1 className="mt-3 text-2xl text-center font-extrabold">{name}</h1>
-      <h4 className="mt-3 text-lg text-center font-extrabold text-violet-300">
-        {description}
-      </h4>
-      <CartButton onClick={() => addToCart(menuItem)} price={price} />
+      {config.ShowImages && (
+        <Image
+          width={192}
+          height={144}
+          src={images}
+          alt="Images"
+          className="rounded-lg mx-auto mt-3"
+        />
+      )}
+      {config.ShowName && (
+        <h1 className="mt-3 text-2xl text-center font-extrabold">{name}</h1>
+      )}
+      {config.ShowDescription && (
+        <h4 className="mt-3 text-lg text-center font-extrabold text-violet-300">
+          {description}
+        </h4>
+      )}
+      {config.ShowPrice && (
+        <CartButton onClick={() => addToCart(menuItem)} price={price} />
+      )}
+      {config.ShowCategory && (
+        <div className="flex justify-center items-center bg-purple-300 w-[40%] mx-auto mt-2 mb-4 rounded-md">
+          <span className="text-xl mb-3 text-white pt-3">{category.name}</span>
+        </div>
+      )}
     </div>
   );
 }
