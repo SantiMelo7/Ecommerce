d0272ea10c06bf3914b6eccb538d6cc964c0738e
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 18:40:51 -0600

    Añadiendo el componente de los botones de edit y delete de las categorias

--- src/components/form/CategoriesPost.jsx
+++ src/components/form/CategoriesPost.jsx
@@ -2,18 +2,17 @@
 
 import useCategoriesRequest from "@/hooks/useCategoriesRequest";
 import NoResults from "@/components/layout/NoResults";
-import { DeleteIcon, EditIcon } from "@/components/layout/Icons";
+import ButtonsCategories from "../button/ButtonsCategories";
 
 export default function CategoriesPost() {
-  const { categories, setEdited, handleDelete, setCategoriesName } =
-    useCategoriesRequest();
+  const { categories } = useCategoriesRequest();
 
   return (
     <section className="mt-7 flex flex-col gap-x-5 hover:transition">
       <NoResults item={categories} />
       {categories?.length > 0 &&
         categories.map((text) => (
-          <div key={text._id}>
+          <div key={text._id} className="grid md:grid-cols-2">
             <div className="flex flex-col items-center mb-4 bg-orange-300 w-[30vh] mx-auto hover:scale-105 rounded-md">
               <div className="grow rounded-md">
                 <h2 className="text-gray-800 text-xl font-bold mb-3 pl-3 pt-3">
@@ -21,19 +20,11 @@ export default function CategoriesPost() {
                 </h2>
               </div>
             </div>
-            <div className="flex gap-8 justify-center items-center">
-              <button
-                className="w-8 h-11"
-                onClick={() => {
-                  setEdited(text), setCategoriesName(text.name);
-                }}
-              >
-                <EditIcon />
-              </button>
-              <button className="pr-4" onClick={() => handleDelete(text._id)}>
-                <DeleteIcon />
-              </button>
-            </div>
+            <ButtonsCategories
+              textName={text}
+              name={text.name}
+              deleteName={text._id}
+            />
           </div>
         ))}
     </section>
