3385189a775c3b3b9e6005b31d1f49acffbc2eed
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-19 15:58:27 -0600

    Añadiendo type y required

--- src/models/ProductsItem.js
+++ src/models/ProductsItem.js
@@ -2,11 +2,11 @@ import { Schema, model, models } from "mongoose"
 import { CategoriesSchema } from "./Categories"
 
 const ProductsItemsSchema = new Schema({
-    name: String,
-    images: String,
-    price: String,
-    description: String,
-    category: CategoriesSchema,
+    name: { type: String, required: true },
+    images: { type: String, required: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: CategoriesSchema, required: true },
 }, { timestamps: true })
 
-export const ProductsItem = models?.ProductsItem || model("ProductsItem", ProductsItemsSchema)
+export const ProductsItem = models?.ProductsShop || model("ProductsShop", ProductsItemsSchema)
