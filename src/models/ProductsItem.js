c9f8aaa0495aaf1380aa0128ba0d38ced93616e9
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-18 21:00:15 -0600

    Cambiando a String

--- src/models/ProductsItem.js
+++ src/models/ProductsItem.js
@@ -2,18 +2,11 @@ import { Schema, model, models } from "mongoose"
 import { CategoriesSchema } from "./Categories"
 
 const ProductsItemsSchema = new Schema({
-
-    name: { type: String, required: true },
-
-    images: { type: String, required: true },
-
-    price: { type: String, required: true },
-
-    description: { type: String, required: true },
-
-    category: { type: CategoriesSchema, required: true },
-
+    name: String,
+    images: String,
+    price: String,
+    description: String,
+    category: CategoriesSchema,
 }, { timestamps: true })
-
 
 export const ProductsItem = models?.ProductsItem || model("ProductsItem", ProductsItemsSchema)
