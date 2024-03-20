dff9a102c05a655bfed9f7db7993fd63cd15299b
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-19 22:14:48 -0600

    Añadiendo la route de el subscribe

---
+++ src/app/api/subscribe/route.js
@@ -0,0 +1,21 @@
+import { SubscribeItem } from "@/models/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { connectMongoDB } from "@/util/connectMongoDB";
+
+export async function PUT(req) {
+    await connectMongoDB()
+    const data = await req.json();
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    const doc = await SubscribeItem.updateOne({ email }, data)
+    return Response.json(doc);
+}
+
+export async function GET() {
+    await connectMongoDB()
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    const user = await SubscribeItem.findOne({ email })
+    return Response.json(user);
+}
