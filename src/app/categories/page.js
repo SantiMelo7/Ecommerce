894e289a689c9ba45040161ddacb62f6cd833da2
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-21 13:57:29 -0600

    Categories

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -5,6 +5,7 @@ import MainTitle from "../../components/layout/MainTitle"
 import useCategoriesRequest from "../../hooks/useCategoriesRequest"
 import { useProducts } from "../../hooks/useProducts"
 import { configAll } from "@/util/constants"
+import LinkProducts from "@/components/products/LinksProducts"
 
 export default function CategoriesHomePage() {
     const { products } = useProducts()
@@ -15,11 +16,12 @@ export default function CategoriesHomePage() {
             <div className="relative">
                 <MainTitle title="All the products" subtTitle="Here you will find the full variety of products we have in more detail." />
             </div>
+            <LinkProducts href="/" text="Volver a la página principal" />
             {categories.length > 0 && categories.map((text) => (
                 <div key={text._id}>
                     <h1 className="title-categories">{text.name}</h1>
                     <section className="grid xl:grid-cols-4 md:grid-cols-2 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 md:max-w-screen-2xl md:mt-10 sm:max-w-screen-sm sm:mx-auto">
-                        {products.filter(item => item.category === text.name).map(item => (
+                        {products.filter(item => item.category.name === text.name).map(item => (
                             <div key={text._id}>
                                 <MenuItemProps {...item} config={configAll} />
                             </div>
