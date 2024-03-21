0c34c478381f471d90060e01dc514fb438e27dff
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-20 20:56:56 -0600

    Añadiendo el link de inicio

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import MainTitle from "@/components/layout/MainTitle"
+import LinkProducts from "@/components/products/LinksProducts"
 import MenuPost from "@/components/products/MenuPost"
 import { configAll } from "@/util/constants"
 
@@ -8,6 +9,7 @@ export default function ProductsHomePage() {
     return (
         <section className="mb-5">
             <MainTitle title="All the products" subtTitle="Here you can see the entire variety of products we have available." />
+            <LinkProducts href="/" text="Volver a la página principal" />
             <MenuPost config={configAll} />
         </section>
     )
