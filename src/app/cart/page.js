395fb3d934e2f7ff0ec17b5cf2c05d8eb430ef3d
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-20 20:57:19 -0600

    Falta solucionarlo

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,24 +2,28 @@
 
 import MainTitle from "@/components/layout/MainTitle";
 import ProductsCart from "@/components/cart/ProductsCart";
-import { useCart } from "@/hooks/useCart";
+import LinkProducts from "@/components/products/LinksProducts";
 import FormCart from "@/components/cart/FormCart";
-import { useProfile } from "@/hooks/useProfile";
+import { useCart } from "@/hooks/useCart";
 import { useSession } from "next-auth/react";
 
 export default function PageCart() {
-    const session = useSession()
-    const { user } = useProfile()
+    const session = useSession();
     const { handleSubmit, address } = useCart();
     return (
-        <>
+        <div>
             <MainTitle title="Cart" subtTitle="Here you have your added products, make your purchase" />
-            <aside className="flex flex-col md:left-6 relative">
-                <ProductsCart />
-            </aside>
-            <div className="flex justify-center items-center">
-                <FormCart profile={session ? user : address} onSubmit={handleSubmit} />
+            <div className="relative md:bottom-5 sm:bottom-9">
+                <LinkProducts href="/" text="Volver a la página principal" />
             </div>
-        </ >
+            <div className="flex sm:flex-col md:flex-row sm:justify-center sm:items-center md:justify-between">
+                <div className="relative md:bottom-20">
+                    <ProductsCart />
+                </div>
+                <div className="relative md:right-12 sm:mt-6">
+                    <FormCart profile={address} onSubmit={handleSubmit} />
+                </div>
+            </div>
+        </div>
     );
 }
