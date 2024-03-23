f4e2977e993fbc300cf41b34c2dedad7fe3c755a
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-22 18:11:03 -0600

    Pagina de las ordenes

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,5 +1,25 @@
+"use client"
+
+import NavTabs from "@/components/layout/NavTabs";
+import { useEffect, useState } from "react";
+
 export default function OrdersPage() {
+    const [orderItems, setOrderItems] = useState([])
+    console.log(orderItems)
+    useEffect(() => {
+        fetch("/api/checkout").then(response => {
+            response.json().then(data => {
+                setOrderItems(data)
+            })
+        })
+    }, [])
     return (
-        <div>Hola</div>
+        <section className="mt-5">
+            <NavTabs isAdmin={true} />
+            {orderItems.length > 0 &&
+                orderItems.map((text) => (
+                    <p>{text.name}</p>
+                ))}
+        </section>
     )
 }
