d2c36e4efca472c32641ba35142d221a5533d8dc
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-22 19:55:29 -0600

    Generico

--- src/hooks/useImageCloudinary.js
+++ src/hooks/useImageCloudinary.js
@@ -7,7 +7,6 @@ export default function useImageCloudinary(setLink) {
     async function handleFile(ev) {
         const files = ev.target.files
         const data = new FormData()
-        //const api = mongoose.connect(process.env.CLOUDINARY_IMAGE)
         data.append("file", files[0])
         data.append("upload_preset", "images")
         setLoanding(true)
@@ -30,47 +29,3 @@ export default function useImageCloudinary(setLink) {
     }
     return { loanding, handleFile }
 }
-/*
-    const files = ev.target.files
-
-        const data= new FormData()
-        data1.append("file", files[0])
-        data1.append("upload_preset", "Products App Shop Restaurant")
-
-        const data2 = new FormData()
-        data2.append("file", files[0])
-        data2.append("upload_preset", "Images About Restaurant")
-
-        setLoanding(true)
-
-        try {
-
-            const response1 = await fetch("https://api.cloudinary.com/v1_1/dqprmrwka/upload", {
-                method: "POST", body: data1
-            })
-            const response2 = await fetch("https://api.cloudinary.com/v1_1/dqprmrwka/upload", {
-                method: "POST", body: data1
-            })
-
-            const file1 = await response1.json()
-            const file2 = await response2.json()
-            setLink(file1.secure_url, file2.secure_url)
-
-            setLoanding(false)
-            // si el file pasa
-            const file = file1 && file2
-
-            if(file) {
-                // retornamos ese file, que lo vuelva json, le damos el then
-                // damos el link como argumento
-                // si pasa damos un segundo estado donde damos que se muestre el link
-                return file.json().then(link => {
-                    setLink(link)
-                })
-            }
-
-        } catch (error) {
-            throw new error("Fallo esto")
-        }
-
-*/
--- src/models/Suscribe.js
+++ src/models/Suscribe.js
@@ -1,7 +1,6 @@
 import { Schema, model, models } from "mongoose"
 
 const SubscribeSchema = new Schema({
-    name: { type: String, required: true },
     email: { type: String, required: true },
 }, { timestamps: true })
 
