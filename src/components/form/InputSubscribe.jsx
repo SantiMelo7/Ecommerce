ee4eab0a0e2dc067891f84b82c52a17c7c59b8bc
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-23 16:06:45 -0600

    Añadiendo el name

--- src/components/form/InputSubscribe.jsx
+++ src/components/form/InputSubscribe.jsx
@@ -6,12 +6,21 @@ import { useSession } from "next-auth/react";
 
 export default function InputSubscribe({ onSubmit, profile }) {
   const session = useSession();
-  const { handleUpdate } = useUpdatedProduct(profile);
+  const { updatedProduct, handleUpdate } = useUpdatedProduct(profile);
   return (
     <form
       className="flex md:flex-row sm:flex-col justify-center items-center gap-x-10"
       onSubmit={(ev) => onSubmit(ev, handleUpdate)}
     >
+      <GenericInput
+        className="w-[40vh]"
+        label="Name"
+        id="name"
+        name="name"
+        type="name"
+        value={updatedProduct?.name || ""}
+        onChange={(ev) => handleUpdate("name", ev.target.value)}
+      />
       <GenericInput
         className="w-[40vh]"
         label="Email"
