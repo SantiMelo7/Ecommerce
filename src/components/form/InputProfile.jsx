2992ac77f52fd01e6eeec0065e219085d8f21314
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-22 19:20:10 -0600

    Añadiendo directamente los inputs de email y name

--- src/components/form/InputProfile.jsx
+++ src/components/form/InputProfile.jsx
@@ -1,9 +1,11 @@
 import useUpdatedProduct from "@/hooks/useUpdatedProduct";
 import ButtonCreate from "../button/ButtonCreate";
 import AddressInput from "./AddressInput";
-import InputNameAndEmail from "./InputNameAndEmail";
+import { useSession } from "next-auth/react";
+import GenericInput from "./GenericInput";
 
 export default function InputProfile({ onSubmit, profile }) {
+  const session = useSession();
   const { updatedProduct, handleUpdate } = useUpdatedProduct(profile);
   const updateAddress = {
     phone: updatedProduct?.phone,
@@ -18,7 +20,26 @@ export default function InputProfile({ onSubmit, profile }) {
         className="flex flex-col justify-center items-center max-w-screen-2xl mx-auto"
         onSubmit={(ev) => onSubmit(ev, updatedProduct)}
       >
-        <InputNameAndEmail doc={{ name: updatedProduct?.name }} />
+        <div className="md:grid md:grid-cols-2 mx-auto md:relative gap-x-3">
+          <GenericInput
+            className="md:w-[34vh] sm:w-[40vh]"
+            label="Email"
+            id="email"
+            name="email"
+            disabled={true}
+            type="email"
+            value={session?.data?.user?.email || ""}
+          />
+          <GenericInput
+            className="md:w-[34vh] sm:w-[40vh]"
+            label="Name"
+            type="text"
+            id="name"
+            value={updatedProduct?.name || ""}
+            onChange={(ev) => handleUpdate("name", ev.target.value)}
+            name="name"
+          />
+        </div>
         <AddressInput address={updateAddress} setAddress={handleUpdate} />
         {updatedProduct?.admin ? (
           <label>
