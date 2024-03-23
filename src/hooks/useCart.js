1781717141bddc2943db6458b219a6209f1b9801
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-22 19:20:22 -0600

    A

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,13 +1,29 @@
 import { CartContext } from "@/context/AppProvider";
 import { useRouter } from "next/navigation";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useProfile } from "./useProfile";
 
 export function useCart() {
     const { cartProducts } = useContext(CartContext);
     const { user } = useProfile()
     const [error, setError] = useState(null)
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [orderItems, setOrderItems] = useState([])
     const router = useRouter()
+    useEffect(() => {
+        fetch("/api/profile").then((response) => {
+            response.json().then((data) => {
+                setIsAdmin(data);
+            });
+        });
+    }, []);
+    useEffect(() => {
+        fetch("/api/checkout").then(response => {
+            response.json().then(data => {
+                setOrderItems(data)
+            })
+        })
+    }, [])
     async function handleSubmitNewOrders(ev, orderUser) {
         ev.preventDefault();
         try {
@@ -26,5 +42,5 @@ export function useCart() {
             router.push("/cart")
         }
     }
-    return { handleSubmitNewOrders, error, user }
+    return { handleSubmitNewOrders, error, user, isAdmin, orderItems }
 }
