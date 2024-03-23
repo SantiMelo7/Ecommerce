7cba0d88a0c2822bbe5719aabf670ca0edd5d3e3
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-22 20:34:16 -0600

    Haciendo un hook de el ampeo pero no qeda definido aun

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,37 +1,18 @@
 "use client"
 
 import NavTabs from "@/components/layout/NavTabs";
+import OrderMap from "@/components/products/OrderMap";
 import { useCart } from "@/hooks/useCart";
 
 export default function OrdersPage() {
-
-    const { orderItems, isAdmin } = useCart()
+    const { orderItems } = useCart()
+    console.log(orderItems)
     return (
         <div className="mt-5">
             <NavTabs />
             <section className="grid md:grid-cols-4 md:pl-4 md:pr-4 sm:grid-cols-1 gap-7 w-full h-full md:max-w-screen-2xl md:mt-10 sm:max-w-screen-sm sm:mx-auto rounded-md">
-                {orderItems.length > 0 &&
-                    orderItems.map((text) => (
-                        <div key={text._id}>
-                            {isAdmin ? <div className={`flex flex-col shadow-lg `}>
-                                <img
-                                    width={200}
-                                    height={200}
-                                    src={text.cartProducts.images}
-                                    alt="Images"
-                                    className="rounded-lg mx-auto mt-3"
-                                />
-                                <h1 className="mt-3 text-2xl text-center font-extrabold">{text.cartProducts.name}</h1>
-                                <h4 className="mt-3 text-lg text-center font-extrabold text-violet-300">
-                                    {text.cartProducts.description}
-                                </h4>
-                                <div className="flex justify-center items-center bg-purple-300 w-[40%] mx-auto mt-2 mb-4 rounded-md">
-                                    <span className="text-xl mb-3 text-white pt-3">{text.cartProducts.category}</span>
-                                </div>
-                                <span className="text-xl font-extrabold text-center">{text.orderUser.name}</span>
-                            </div> : null}
-                        </div>
-                    ))}
+                <pre>{JSON.stringify(orderItems, null, 2)}</pre>
+                <OrderMap />
             </section>
         </div>
     )
---
+++ src/components/products/OrderMap.jsx
@@ -0,0 +1,45 @@
+import { useCart } from "@/hooks/useCart";
+import MainTitle from "../layout/MainTitle";
+
+export default function OrderMap() {
+  const { orderItems, isAdmin } = useCart();
+  return (
+    <>
+      {orderItems.length > 0 &&
+        orderItems.map((text, index) => (
+          <div key={index}>
+            {isAdmin ? (
+              <div className={`flex flex-col shadow-lg `}>
+                <img
+                  width={200}
+                  height={200}
+                  src={text.cartProducts.images}
+                  alt="Images"
+                  className="rounded-lg mx-auto mt-3"
+                />
+                <h1 className="mt-3 text-2xl text-center font-extrabold">
+                  {text.cartProducts.name}
+                </h1>
+                <h4 className="mt-3 text-lg text-center font-extrabold text-violet-300">
+                  {text.cartProducts.description}
+                </h4>
+                <div className="flex justify-center items-center bg-purple-300 w-[40%] mx-auto mt-2 mb-4 rounded-md">
+                  <span className="text-xl mb-3 text-white pt-3">
+                    {text.cartProducts.category}
+                  </span>
+                </div>
+                <span className="text-xl font-extrabold text-center">
+                  {text.orderUser.name}
+                </span>
+              </div>
+            ) : (
+              <MainTitle
+                title="Gracias por tu compra, te damos una revision de tu pedido"
+                subtTitle=""
+              />
+            )}
+          </div>
+        ))}
+    </>
+  );
+}
