df7937cf77452dafd3e4ea6afee0fd0946af8e38
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-28 22:41:33 -0600

    Eliminando el subscribe

--- src/models/Suscribe.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model, models } from "mongoose"
-
-const SubscribeSchema = new Schema({
-    email: { type: String, required: true },
-}, { timestamps: true })
-
-export const SubscribeItem = models?.suscribeItem || model("suscribeItem", SubscribeSchema)
